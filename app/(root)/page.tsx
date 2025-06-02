ca4fe2a53a8a1b851676ec0068ae04ef2684c08b
john.f <john.florendo@revolve.com>
2025-06-02 20:08:19 +0800

    NextJS Store: Initial commit + Tailwind4 + assets

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button';
+
+const Homepage = () => {
+  // return <>ProStore</>
+  return <Button>My Button</Button>
+}
+
+export default Homepage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'ProStore2';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'eCommerce site built from nextJS 2025';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
